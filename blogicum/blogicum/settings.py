75dd4d2454274e03ffa8968072ed3af7a8313f7e
Vitaly Nukeev <design-tri@yandex.ru>
2025-03-29 23:51:03 +0500

    v2

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -128,10 +128,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
-
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static/css",
-    BASE_DIR / "static/img",
+    BASE_DIR / "static"
 ]
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')), 
-    path('pages/', include('pages.urls')),
+    path('', include('blog.urls', namespace='blog')),
+    path('pages/', include('pages.urls', namespace='pages')),
 ]
