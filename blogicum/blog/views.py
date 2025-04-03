4ef291da624822889285841b0a0f8d405af7acc8
Vitaly Nukeev <design-tri@yandex.ru>
2025-04-03 21:59:23 +0500

    v5

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -43,15 +43,15 @@ posts = [
 
 
 def index(request):
-    return render(request, 'index.html', {'posts': posts})
+    return render(request, 'blog/index.html', {'posts': posts})
 
 
 def post_detail(request, id):
     post = next((post for post in posts if post['id'] == id), None)
     if post is None:
         raise Http404('Post not found')
-    return render(request, 'detail.html', {'post': post})
+    return render(request, 'blog/detail.html', {'post': post})
 
 
 def category_posts(request, category_slug):
-    return render(request, 'category.html', {'category_slug': category_slug})
+    return render(request, 'blog/category.html', {'category_slug': category_slug})
